246f7f1837acefc2af2bd86843beaeb670186e79
Rene Groeschke <rene@breskeby.com>
2012-05-16 13:49:00 +0200

    Add snippet that shows how to rerun failed tests using testng

---
+++ testing/RerunFailedTestsWithTestNG/src/test/java/org/acme/SimpleTest.java
@@ -0,0 +1,24 @@
+package org.acme;
+ 
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class SimpleTest {
+ 
+ @BeforeClass
+ public void setUp() {
+   // code that will be invoked when this test is instantiated
+ }
+ 
+ @Test(groups = { "fast" })
+ public void aFastTest() {
+   System.out.println("Fast test");
+ }
+ 
+ @Test(groups = { "slow" })
+ public void aSlowTest() {
+    System.out.println("Slow test");
+    //fail();
+ }
+
+}
